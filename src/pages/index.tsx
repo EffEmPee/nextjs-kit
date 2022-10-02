56896885970ae20d33eba7dc2d364b3b52146430
felipe pierotti <felipe@DESKTOP-NBU16Q3.localdomain>
2022-10-02 19:12:12 -0300

    feat: remove toast dependency

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,33 +1,16 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
-import { toast } from 'react-toastify';
-
-import 'react-toastify/dist/ReactToastify.css';
-import { StyledToastContainer } from '../styles/toastStyle';
 
 import { Container } from '../styles/pages/Home';
 
-const Home: NextPage = () => {
-  const handleHotToastSuccess = () => toast.success('Sucesso!');
-  const handleHotToastError = () => toast.error('Erro!');
+const Home: NextPage = () => (
+  <Container>
+    <Head>
+      <title>NextJS kit</title>
+    </Head>
 
-  return (
-    <>
-      <StyledToastContainer />
-      <Container>
-        <Head>
-          <title>Toast notify</title>
-        </Head>
-
-        <button type="button" onClick={handleHotToastSuccess}>
-          <span>success</span>
-        </button>
-        <button type="button" onClick={handleHotToastError}>
-          <span>error</span>
-        </button>
-      </Container>
-    </>
-  );
-};
+    <h1>Hello world!</h1>
+  </Container>
+);
 
 export default Home;
